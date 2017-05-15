4c68fb99127c9efdf048ef35eb99dfcbaa8ffb63
MeksPwnschlager <doctoranyk@gmail.com>
2017-05-15 20:26:04 +0200

    RouteMenu.vue

---
+++ src/src/components/RouteMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div></div>
+      <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+// import * as types from '../store/mutation-types'
+
+export default {
+  computed: mapGetters([
+    'getRouteSelected' // TBD
+  ]),
+  methods: {
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
